262e7536c6fa909f088cea740526860456a02e81
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2021-04-21 11:37:33 +0200

    Moved some more deps to trustinsoft/deps

---
+++ trustinsoft/deps/cmake_build/bl2/ext/mcuboot/CMakeFiles/signing_layout_ns.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for signing_layout_ns.
+# This may be replaced when dependencies are built.
---
+++ trustinsoft/deps/cmake_build/bl2/ext/mcuboot/CMakeFiles/signing_layout_s.dir/signing_layout_s.o.d
@@ -0,0 +1,3 @@
+bl2/ext/mcuboot/CMakeFiles/signing_layout_s.dir/signing_layout_s.o: \
+ /home/qba/git/trusted-firmware-m/cmake_build/bl2/ext/mcuboot/signing_layout_s.c \
+ /home/qba/git/trusted-firmware-m/platform/ext/target/mps2/an521/partition/flash_layout.h
